bbbf1b598e776f667e66d2d70298b3bbf96d774a
zouwx2cs <31923541+zouwx2cs@users.noreply.github.com>
2018-01-25 17:28:10 +0800

    Add files via upload

---
+++ 1000-1999/OpenJudge1000.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std ;
+
+int main(int argc, char* argv[]) 
+{
+	cin >> a >> b ;
+	cout << a+b << endl ;
+	return 0 ;
+}
---
+++ 1000-1999/OpenJudge1000.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+# -*- coding: utf-8 -*-
+i = input().split()
+print(int(i[0]) + int(i[1]))
+
